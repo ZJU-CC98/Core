9747487ff3ae3731d83aeec832009082f75b83f9
Iris Sakura <sgjsakura@hotmail.com>
2023-03-10 15:57:57 +0800

    Update

--- CC98.Core/CC98.Services.ContentCheck.Web/Data/ContentCheckDbContext.cs
+++ CC98.Core/CC98.Services.ContentCheck.Web/Data/ContentCheckDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+
 using CC98.Services.ContentCheck.Data.Configuration;
 
 using Microsoft.EntityFrameworkCore;
@@ -14,30 +15,36 @@ namespace CC98.Services.ContentCheck.Data;
 /// </summary>
 internal class ContentCheckDbContext : DbContext
 {
-    /// <summary>
-    /// 获取或设置审核结果的集合。
-    /// </summary>
-    public virtual required DbSet<ContentCheckItem> ContentCheckItems { get; set; }
+	public ContentCheckDbContext(DbContextOptions<ContentCheckDbContext> options)
+		: base(options)
+	{
 
-    /// <summary>
-    ///获取或设置审核操作记录的集合。
-    /// </summary>
-    public virtual required DbSet<ContentCheckOperationRecord> ContentCheckOperationRecords { get; set; }
+	}
 
-    /// <inheritdoc />
-    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
-    {
-        // 辅助方法
-        static ProviderConventionSetBuilderDependencies GetDependencies(IServiceProvider sp)
-        {
-            return sp.GetRequiredService<ProviderConventionSetBuilderDependencies>();
-        }
+	/// <summary>
+	/// 获取或设置审核结果的集合。
+	/// </summary>
+	public virtual required DbSet<ContentCheckItem> ContentCheckItems { get; set; }
 
-        base.ConfigureConventions(configurationBuilder);
+	/// <summary>
+	///获取或设置审核操作记录的集合。
+	/// </summary>
+	public virtual required DbSet<ContentCheckOperationRecord> ContentCheckOperationRecords { get; set; }
 
-        // 添加自定义转换
-        configurationBuilder.Conventions.Add(sp => new ExcludeFromMigrationsAttributeConvention(GetDependencies(sp)));
-        configurationBuilder.Conventions.Add(sp => new DefaultValueAttributeConvention(GetDependencies(sp)));
-        configurationBuilder.Conventions.Add(sp => new DiscriminatorAttributeConvention(GetDependencies(sp)));
-    }
+	/// <inheritdoc />
+	protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+	{
+		// 辅助方法
+		static ProviderConventionSetBuilderDependencies GetDependencies(IServiceProvider sp)
+		{
+			return sp.GetRequiredService<ProviderConventionSetBuilderDependencies>();
+		}
+
+		base.ConfigureConventions(configurationBuilder);
+
+		// 添加自定义转换
+		configurationBuilder.Conventions.Add(sp => new ExcludeFromMigrationsAttributeConvention(GetDependencies(sp)));
+		configurationBuilder.Conventions.Add(sp => new DefaultValueAttributeConvention(GetDependencies(sp)));
+		configurationBuilder.Conventions.Add(sp => new DiscriminatorAttributeConvention(GetDependencies(sp)));
+	}
 }
