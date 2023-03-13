386a4b2ba0b6d9b06b8b4b6ba29d00d7b028c184
Iris Sakura <sgjsakura@hotmail.com>
2023-03-13 14:44:49 +0800

    Update

--- CC98.Core/CC98.Services.ContentCheck.Web/ContentCheckRecordMode.cs
+++ CC98.Core/CC98.Services.ContentCheck.Web/ContentCheckRecordMode.cs
@@ -1,4 +1,6 @@
-﻿namespace CC98.Services.ContentCheck;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CC98.Services.ContentCheck;
 
 /// <summary>
 ///     定义内容审查服务执行时对结果的记录模式。
@@ -8,15 +10,18 @@ public enum ContentCheckRecordMode
 	/// <summary>
 	///     使用默认记录模式。也即当结果严重性到达系统后台设置的严重性级别时，记录结果；否则不记录结果。
 	/// </summary>
+	[Display(Name = "使用系统默认设置", ShortName = "默认")]
 	Default = 0,
 
 	/// <summary>
 	///     始终记录结果。
 	/// </summary>
+	[Display(Name = "始终记录审查结果", ShortName = "始终")]
 	Always,
 
 	/// <summary>
 	///     始终不记录结果。
 	/// </summary>
+	[Display(Name = "从不记录检查结果", ShortName = "从不")]
 	Never
 }
